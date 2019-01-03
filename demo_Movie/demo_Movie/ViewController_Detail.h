1839ce7e20f4f24fb29e09a5a01d09d65737eeb7
ann60319 <34393480+ann60319@users.noreply.github.com>
2019-01-03 11:10:58 +0800

    Add files via upload

---
+++ demo_Movie/demo_Movie/ViewController_Detail.h
@@ -0,0 +1,17 @@
+//
+//  ViewController_Detail.h
+//  demo_Movie
+//
+//  Created by Yi Hwei Huang on 2019/1/2.
+//  Copyright © 2019 Yi Hwei Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ViewController_Detail : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ demo_Movie/demo_Movie/ViewController_Detail.m
@@ -0,0 +1,32 @@
+//
+//  ViewController_Detail.m
+//  demo_Movie
+//
+//  Created by Yi Hwei Huang on 2019/1/2.
+//  Copyright © 2019 Yi Hwei Huang. All rights reserved.
+//
+
+#import "ViewController_Detail.h"
+
+@interface ViewController_Detail ()
+
+@end
+
+@implementation ViewController_Detail
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
